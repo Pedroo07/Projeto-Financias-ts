ffd90b6cfba16053b858f77dd5b8d82dbd0702ce
Pedroo <pedrohenriquemoalive@gmail.com>
2024-01-25 12:21:41 -0300

    Pegando data atual e atualizando a lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useState, useEffect} from 'react'
 import * as C from './App.styles'
-
+import {Item} from './types/Item'
+import {Category} from './types/Category'
+import {categories} from './data/categories'
+import {items} from './data/items'
+import {getCurrentMonth} from './helpers/dateFilter'
 const App = () => {
+
+  const [list, setList]= useState(items)
+  const [filterList, setFilterList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+useEffect(() => {
+
+}, [list,currentMonth])
   return (
     <C.Container>
       <C.Header>
