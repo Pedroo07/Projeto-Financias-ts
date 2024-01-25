35731cbc27b1fbc4ba5b505bf322ba31c1cde339
Pedroo <pedrohenriquemoalive@gmail.com>
2024-01-25 12:56:00 -0300

    Filtrando  lista pelo mês e ano atual.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import {Item} from './types/Item'
 import {Category} from './types/Category'
 import {categories} from './data/categories'
 import {items} from './data/items'
-import {getCurrentMonth} from './helpers/dateFilter'
+import {getCurrentMonth,filteredList} from './helpers/dateFilter'
 const App = () => {
 
   const [list, setList]= useState(items)
   const [filterList, setFilterList] = useState<Item[]>([])
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
 useEffect(() => {
-
+  setFilterList(filteredList(list, currentMonth))
 }, [list,currentMonth])
   return (
     <C.Container>
