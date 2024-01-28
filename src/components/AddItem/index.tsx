c4469a7e3f63c116c84a1a6e122bd2c47229d42a
Pedroo <pedrohenriquemoalive@gmail.com>
2024-01-27 22:05:49 -0300

    Criando logica para adicionar Item na lista com data valores e titulo

--- src/components/AddItem/index.tsx
+++ src/components/AddItem/index.tsx
@@ -1,24 +1,85 @@
+import React, { useState } from 'react'
 import { Item } from '../../types/Item'
 import * as C from './styles'
 
-type Props ={
+type Props = {
     onAdd: (item: Item) => void
 }
 
-export const AddItem = ({onAdd}: Props) => {
+export const AddItem = ({ onAdd }: Props) => {
+    const [inputTitle, setInputTitle] = useState<string>('')
+    const [inputDate, setInputDate] = useState<Date>()
+    const [inputValue, setInputValue] = useState<number>(0)
+    const [inputSelect, setInputSelect] = useState<string>('')
+
+    const inputTitleCommand = (event: React.ChangeEvent<HTMLInputElement>) => setInputTitle(event.target.value)
+    const inputValueCommand = (event: React.ChangeEvent<HTMLInputElement>) => setInputValue(Number(event.target.value))
+    const inputDateCommand = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const dateValue = event.target.value
+        const parsedDate = new Date(dateValue)
+        setInputDate(parsedDate)
+    }
 
     const handleEvent = () => {
-        let newItem: Item = {
-            date: new Date(2024,1,9),
-            category: 'gasto',
-            title: 'Adega 22',
-            value: 40.00
+        if (!inputDate) {
+            alert('Por favor, selecione uma data.');
+            return;
+        }if (!inputTitle){
+            alert('Por Favor adicione um titulo')
+            return
+        }if (!inputValue){
+            alert('Por Favor adicione um valor')
+            return
+        }if(inputSelect === ''){
+            alert('Por Favor selecione uma categoria')
         }
-        onAdd(newItem)
+
+        let newItem: Item = {
+            date: inputDate,
+            category: inputSelect,
+            title: inputTitle,
+            value: inputValue
+        }
+
+        onAdd(newItem);
     }
-    return(
-    <C.Container>
-        <button onClick={handleEvent}>Add</button>
-    </C.Container>
-    )    
+
+    return (
+        <C.Container>
+            <C.Div>
+                <h3>Data</h3>
+                <C.InputItem
+                    type='date'
+                    value={inputDate?.toISOString().split('T')[0]}
+                    onChange={inputDateCommand}
+                />
+            </C.Div>
+            <C.Div>
+                <h3>Categoria</h3>
+                {/* Adicione a lógica para selecionar a categoria */}
+                <C.SelectItem value={inputSelect} onChange={(e) => setInputSelect(e.target.value) } >
+                    <option value=""></option>
+                    <option value="renda">Renda</option>
+                    <option value="gasto">Despesas</option>
+                </C.SelectItem>
+            </C.Div>
+            <C.Div>
+                <h3>Título</h3>
+                <C.InputItem
+                    type='text'
+                    value={inputTitle}
+                    onChange={inputTitleCommand}
+                />
+            </C.Div>
+            <C.Div>
+                <h3>Valor</h3>
+                <C.InputItem
+                    type='number'
+                    value={inputValue}
+                    onChange={inputValueCommand}
+                />
+            </C.Div>
+            <C.Button onClick={handleEvent}>Adicionar</C.Button>
+        </C.Container>
+    )
 }
--- src/components/AddItem/styles.ts
+++ src/components/AddItem/styles.ts
@@ -1,9 +1,43 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+align-items: center;
+gap: 0.7rem;
 background-color: white;
 box-shadow: 0 0 5px #ccc;
 border-radius: 10px;
 padding: 20px;
 margin-top: 20px;
+`
+export const InputItem = styled.input`
+padding: 5px;
+font-size: 1.1rem;
+font-weight: 500;
+border:1px solid lightblue;
+`
+export const SelectItem = styled.select`
+font-weight: 500;
+padding: 5px;
+font-size: 1.1rem;
+border: 1px solid lightblue;
+
+`
+export const Div = styled.div`
+display: flex;
+flex-direction: column;
+`
+export const Button = styled.button`
+margin-top: 18px;
+border: none;
+border-radius: 8px;
+cursor: pointer;
+font-size: 1.05rem;
+width: 10rem;
+padding: 5px;
+margin-right: 1px;
+background-color: lightblue;
+color: black;
 `
